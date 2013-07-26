20e5141d02b238820542ec032d4936948a054ae5
Yuriy Marchenko <yuriy.marchenko@fu-berlin.de>
2013-07-26 11:36:18 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stolpersteine::Application.config.secret_key_base = '26484cdfcb5a5886b8cfba57370dcb7d5f7df841d45a90b1b70d6bcac0ba1c4b38c14f4e69626b76009ea0c24931cb20da2be2543a671b1cebb5f8c47c4576c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stolpersteine::Application.config.session_store :cookie_store, key: '_stolpersteine_session'
