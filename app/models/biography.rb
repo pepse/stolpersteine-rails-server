e70935727b1510af9f7238c4939cc8948b476b22
Kashif Rasul <kashif.rasul@gmail.com>
2013-07-26 14:55:10 +0200

    as_json WIP

--- app/models/biography.rb
+++ app/models/biography.rb
@@ -1,3 +1,12 @@
 class Biography < ActiveRecord::Base
   belongs_to :location
+
+  def as_json(options = nil)
+    {
+      :created_at => self.created_at
+      :updated_at => self.updated_at
+      :name => self.name
+      :location => self.location.id
+    }
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -6,4 +6,16 @@ class Location < ActiveRecord::Base
 
   # But use a geographic implementation for the :lnglat column.
   set_rgeo_factory_for_column(:lnglat, RGeo::Geographic.spherical_factory(srid: 4326))
+
+
+  def as_json(option = nil)
+    {
+      lng => self.lnglat.x
+      lat => self.lnglat.y
+      bezirk => self.bezirk
+      bio_count => self.biographies.size
+      bio_ids => self.biographies.pluck(:id)
+      ...
+    }
+  end
 end
