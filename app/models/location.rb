f0d589e05b9ee802f2bdb088df5242f99c74fbc2
Patrick Hobusch <patrick.hobusch@gmail.com>
2013-07-26 15:16:10 +0200

    Added new Gem

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,14 +7,13 @@ class Location < ActiveRecord::Base
   # But use a geographic implementation for the :lnglat column.
   set_rgeo_factory_for_column(:lnglat, RGeo::Geographic.spherical_factory(srid: 4326))
 
-
-  def as_json(option = nil)
-    {
-      lng => self.lnglat.x
-      lat => self.lnglat.y
-      bezirk => self.bezirk
-      bio_count => self.biographies.size
-      bio_ids => self.biographies.pluck(:id)
+  def as_json(options = nil)
+   {
+      :lng => self.lnglat.x,
+      :lat => self.lnglat.y,
+      :bezirk => self.bezirk,
+      :bio_count => self.biographies.size,
+      :bio_ids => self.biographies.pluck(:id)
     }
   end
 end
