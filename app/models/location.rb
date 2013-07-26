2b3813634e4699de73f9499ac0de71787415056a
Kashif Rasul <kashif.rasul@gmail.com>
2013-07-26 13:47:57 +0200

    added the rgeo factory for lnglat column

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ActiveRecord::Base
   has_many :biographies, dependent: :destroy
+
+  # By default, use the GEOS implementation for spatial columns.
+  self.rgeo_factory_generator = RGeo::Geos.factory_generator
+
+  # But use a geographic implementation for the :lnglat column.
+  set_rgeo_factory_for_column(:lnglat, RGeo::Geographic.spherical_factory(srid: 4326))
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130726104821) do
+ActiveRecord::Schema.define(version: 20130726112117) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "biographies", force: true do |t|
     t.string   "name"
